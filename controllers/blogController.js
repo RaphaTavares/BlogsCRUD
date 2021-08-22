411cdbbc28a9d24817d3521d43982f3158fc9e2f
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-22 16:17:30 -0300

    Separate blogs views

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,7 +4,7 @@ const Blog = require('../models/blog')
 const blog_index = (req, res) => {
     Blog.find().sort({ createdAt: -1 })
         .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
+            res.render('blogs/index', { title: 'All Blogs', blogs: result })
         })
         .catch(err => console.log(err))
 }
@@ -14,13 +14,13 @@ const blog_details = (req, res) => {
     console.log(id)
     Blog.findById(id)
         .then(result =>{
-            res.render('details', { blog: result, title: 'Blog Details'})
+            res.render('blogs/details', { blog: result, title: 'Blog Details'})
         })
         .catch(err => console.log(err))
 }
 
 const blog_create_get = (req, res) =>{
-    res.render('create', {title: 'Create a blog'})
+    res.render('blogs/create', {title: 'Create a blog'})
 }
 const blog_create_post = (req, res) =>{
     const blog = new Blog(req.body)
